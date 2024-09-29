7c55d6954fd4dff526736c804167903d73cb21c1
Sandeep <sandeepkumar2022000@gmail.com>
2024-09-30 04:58:09 +0530

    sandeep is learning developerment

---
+++ 4.getElementsByTagName/index.js
@@ -0,0 +1,7 @@
+let fruits=document.getElementsByClassName('fruit')
+for(let i=0;i<fruits.length;i++){
+  fruits[i].style.fontStyle='italic'
+}
+let lis=document.getElementsByTagName('li')
+
+lis[4].style.color='blue'
